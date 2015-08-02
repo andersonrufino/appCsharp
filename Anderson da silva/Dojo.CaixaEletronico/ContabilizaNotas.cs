94e1b537fd963057ce75f809c6fa8986524b806d
andersonrufino <anderliny@hotmail.com>
2015-08-02 12:24:08 -0300

    Dojo

---
+++ Anderson da silva/Dojo.CaixaEletronico/ContabilizaNotas.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dojo.CaixaEletronico
+{
+    public class quantidadeDeNotas
+    {
+        private int valorDeSaque = 0;
+        private int notaDeDez = 0;
+        private int notaDeVinte = 0;
+        private int notaDeCinquenta = 0;
+        private int notaDeCem = 0;
+
+        public int Saque
+        {
+            get
+            {
+                return valorDeSaque;
+            }
+            set
+            {
+                this.valorDeSaque = value;
+            }
+        }
+
+        public int Dez
+        {
+            get
+            {
+                return notaDeDez;
+            }
+            set
+            {
+                this.notaDeDez=value;
+            }
+
+        }
+
+        public int Vinte
+        {
+            get
+            {
+                return notaDeVinte;
+            }
+            set
+            {
+                this.notaDeVinte=value;
+            }
+
+        }
+
+        public int cinquenta
+        {
+            get
+            {
+                return notaDeCinquenta;
+            }
+            set
+            {
+                this.notaDeCinquenta=value;
+            }
+        }
+
+        public int Cem
+        {
+            get
+            {
+                return notaDeCem;
+            }
+            set
+            {
+                this.notaDeCem = value;
+            }
+
+        }
+
+        public int ContabilizaCedulas(int valor)
+        {
+            int valorDeSaque = valor;
+             
+            while (valorDeSaque >= 100)
+            {
+                valorDeSaque -= 100;
+                notaDeCem++;
+            }
+
+            while (valorDeSaque >= 50)
+            {
+                valorDeSaque -= 50;
+                notaDeCinquenta++;
+            }
+
+            while (valorDeSaque >= 20)
+            {
+                valorDeSaque -= 20;
+                notaDeVinte++;
+            }
+
+            while (valorDeSaque >= 10)
+            {
+                valorDeSaque -= 10;
+                notaDeDez++;
+            }
+
+            if (valorDeSaque != 0)
+            {
+                throw new ArgumentException("Valor solicitador é invalido!");
+            }
+
+            return 0;
+        }
+
+        public void ContabilizaCedulas(object p)
+        {
+            throw new ArgumentException("Não foi informado valor");
+            
+        }
+
+    }
+}
---
+++ Anderson da silva/Dojo.TesteDoCaixa/TestesDeCedulas.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dojo.CaixaEletronico;
+using System.Diagnostics;
+
+namespace Dojo.TesteDoCaixa
+{ 
+    [TestClass]
+    public class TestesDeCedulas
+    {
+        private quantidadeDeNotas notas;
+
+        [TestInitialize]
+        public void IniciaTesteCaixaBancario()
+        {
+            notas = new quantidadeDeNotas();
+        }
+
+        [TestCleanup]
+        public void FinalizaTesteCaixaBancario()
+        {
+            Debug.WriteLine("fim teste");
+        }
+
+        [TestMethod]
+        public void entrega_valor_10()
+        {
+            notas.ContabilizaCedulas(10);
+            Assert.AreEqual(1, notas.Dez);
+        }
+        
+        [TestMethod]
+        public void entrega_valor_20()
+        {
+            notas.ContabilizaCedulas(20);
+            Assert.AreEqual(1, notas.Vinte);
+        }
+
+        [TestMethod]
+        public void entrega_valor_50()
+        {
+            notas.ContabilizaCedulas(50);
+            Assert.AreEqual(1,notas.cinquenta);
+        }
+
+        [TestMethod]
+        public void entrega_valor_100()
+        {
+            notas.ContabilizaCedulas(100);
+            Assert.AreEqual(1,notas.Cem);
+        }
+
+        [TestMethod]
+        public void entrega_valor_140()
+        {
+            notas.ContabilizaCedulas(140);
+            Assert.AreEqual(1,notas.Cem);
+            Assert.AreEqual(2,notas.Vinte);
+            
+        }
+        
+        [TestMethod]
+        public void entrega_valor_180()
+        {
+            notas.ContabilizaCedulas(180);
+            Assert.AreEqual(1,notas.Cem);
+            Assert.AreEqual(1,notas.cinquenta);
+            Assert.AreEqual(1,notas.Vinte);
+            Assert.AreEqual(1,notas.Dez);
+
+        }
+
+        [TestMethod]
+        public void entrega_valor_230()
+        {
+            notas.ContabilizaCedulas(230);
+            Assert.AreEqual(2,notas.Cem);
+            Assert.AreEqual(1,notas.Vinte);
+            Assert.AreEqual(1, notas.Dez);
+
+        }
+
+        [TestMethod]
+        public void entrega_valor_650()
+        {
+            notas.ContabilizaCedulas(650);
+            Assert.AreEqual(6,notas.Cem);
+            Assert.AreEqual(1,notas.cinquenta);
+
+        }
+
+        [TestMethod]
+        public void entrega_valor_1890()
+        {
+            notas.ContabilizaCedulas(1890);
+            Assert.AreEqual(18, notas.Cem);
+            Assert.AreEqual(1, notas.cinquenta);
+            Assert.AreEqual(2, notas.Vinte);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void testar_caso_nao_seja_passado_nenhum_valor()
+        {
+            notas.ContabilizaCedulas(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void testar_caso_cedula_nao_possua_no_caixa()
+        {
+            notas.ContabilizaCedulas(25);
+         
+        }
+    }
+}
